1b1dc62173d7bbca69d67123c2bb1104c7ee3452
Om Tiwary <143310925+OmTiwary@users.noreply.github.com>
2024-03-30 19:32:49 +0000

    DataTypes Done

---
+++ Basics/02_Datatypes.js
@@ -0,0 +1,23 @@
+"use strict" // Treasts all JS code as newer version.
+
+// alert(3+3)  We are using NodeJS not Browser
+
+let name = "Om Tiwary"  // Anything in between " " or ' ' will be STRING.
+let roll = 25   // Without " " is counted as NUMBER
+let IsLoggedIn = true  // Is Boolean datatype
+
+/* Types of DataTypes
+Number => 2 to the power 53, this is range
+bigint = for very large numbers
+String = '' or ""
+Boolean = True or Flase || 0 or 1
+null = standalone value
+undefined = Value is not given
+symbol = for unique
+*/
+
+
+console.log(typeof "Om");
+console.log(typeof true); // type Boolean
+console.log(typeof null); // type is Object
+console.log(typeof undefined); //type is Undefined
